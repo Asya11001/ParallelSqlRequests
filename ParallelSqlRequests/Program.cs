6c1675136d6272872d3ac0a891fd5fe5ee77f8e6
Arsenii <arseniysemenov031203@gmail.com>
2024-01-21 19:11:34 +0300

    master. It seems it is working fine!

--- ParallelSqlRequests/Program.cs
+++ ParallelSqlRequests/Program.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using MPI;
 using Newtonsoft.Json;
-using Environment = MPI.Environment;
 
 public static class JsonSerializerHelper
 {
@@ -64,27 +63,29 @@ class Program
     {
         using (var dbContext = new SampleDbContext())
         {
-                for (int i = 0; i < numProcesses - 1; i++)
+            for (int i = 0; i < numProcesses - 1; i++)
+            {
+                bool tableExists = dbContext.Database.ExecuteSqlRaw(
+                    $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'computer_hardware_{i}'"
+                ) > 0;
+
+                if (!tableExists)
                 {
-                    bool tableExists = dbContext.Database.ExecuteSqlRaw(
-                        $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'computer_hardware_{i}'"
-                    ) > 0;
+                    Console.WriteLine($"Creating table computer_hardware_{i}...");
+                    var stopwatch = Stopwatch.StartNew();
 
-                    if (!tableExists)
-                    {
-                        Console.WriteLine($"Creating table computer_hardware_{i}...");
+                    dbContext.Database.ExecuteSqlRaw(
+                        $"EXEC('SELECT * INTO computer_hardware_{i} FROM computer_hardware WHERE computer_hardware_id % {numProcesses - 1} = {i};')"
+                    );
 
-                        dbContext.Database.ExecuteSqlRaw(
-                            $"EXEC('SELECT * INTO computer_hardware_{i} FROM computer_hardware WHERE computer_hardware_id % {numProcesses - 1} = {i};')"
-                        );
-
-                        Console.WriteLine($"Table computer_hardware_{i} created.");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Table computer_hardware_{i} already exists.");
-                    }
+                    stopwatch.Stop();
+                    Console.WriteLine($"Table computer_hardware_{i} created. Elapsed Time: {stopwatch.ElapsedMilliseconds} ms");
                 }
+                else
+                {
+                    Console.WriteLine($"Table computer_hardware_{i} already exists.");
+                }
+            }
         }
     }
 
@@ -96,7 +97,10 @@ class Program
             {
                 for (int i = 0; i < numProcesses - 1; i++)
                 {
+                    var stopwatch = Stopwatch.StartNew();
                     dbContext.Database.ExecuteSqlRaw($"DROP TABLE IF EXISTS computer_hardware_{i}");
+                    stopwatch.Stop();
+                    Console.WriteLine($"Dropped table computer_hardware_{i}. Elapsed Time: {stopwatch.ElapsedMilliseconds} ms");
                 }
             }
         }
@@ -107,12 +111,29 @@ class Program
         using (var dbContext = new SampleDbContext())
         {
             var tableName = $"computer_hardware_{rank - 1}";
+            var stopwatch = Stopwatch.StartNew();
             var localData = dbContext.ComputerHardwares
                 .FromSqlRaw($"SELECT * FROM {tableName}")
                 .Select(hardware => JsonSerializerHelper.SerializeObject(hardware))
                 .ToList();
-
+            stopwatch.Stop();
+            Console.WriteLine($"Retrieved local data for process {rank}/{Communicator.world.Size}. Elapsed Time: {stopwatch.ElapsedMilliseconds} ms");
             return localData;
+        }
+    }
+    static void ExecuteMainTableRequest()
+    {
+        using (var dbContext = new SampleDbContext())
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            // Replace the following query with your actual query on the main table
+            var result = dbContext.ComputerHardwares.ToList();
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"[MAIN REQUEST]: Main table request executed. Elapsed Time: {stopwatch.ElapsedMilliseconds} ms");
+            // You can process or print the result as needed
         }
     }
     static void Main(string[] args)
@@ -122,123 +143,48 @@ class Program
             int rank = Communicator.world.Rank;
             int numProcesses = Communicator.world.Size;
 
+            // if (rank == 0)
+            // {
+            //     var stopwatch = Stopwatch.StartNew();
+            //     CreateTable(numProcesses);
+            //     stopwatch.Stop();
+            //     Console.WriteLine($"Table creation for rank 0. Elapsed Time: {stopwatch.ElapsedMilliseconds} ms");
+            // }
+
             if (rank == 0)
             {
-                CreateTable( numProcesses);
+                    ExecuteMainTableRequest();
             }
             if (rank != 0)
             {
                 // Non-zero ranks send their local data to rank 0
                 var localData = GetLocalData(rank);
                 string serializedData = string.Join(",", localData);
-                // Console.WriteLine($"Process {rank}/{numProcesses} sending data: {serializedData}");
                 Console.WriteLine($"Process {rank}/{numProcesses} sending data...");
+                var stopwatch = Stopwatch.StartNew();
                 Communicator.world.Send(serializedData, 0, 0);
-                Console.WriteLine($"Process {rank}/{numProcesses} sent data");
+                stopwatch.Stop();
+                Console.WriteLine($"Process {rank}/{numProcesses} sent data. Elapsed Time: {stopwatch.ElapsedMilliseconds} ms");
             }
             else
             {
                 // Rank 0 gathers data from other processes
                 var gatheredData = new List<string>();
+                var stopwatch = Stopwatch.StartNew();
                 for (int i = 1; i < numProcesses; i++)
                 {
                     Console.WriteLine($"Process {rank}/{numProcesses} receiving data from process {i}/{numProcesses}");
-                    string receivedData = Communicator.world.Receive<string>(i, 0);
-                    // // Console.WriteLine($"Process {rank}/{numProcesses} received data from process {i}/{numProcesses}: {receivedData}");
+                    var receivedData = Communicator.world.Receive<string>(i, 0);
                     Console.WriteLine($"Process {rank}/{numProcesses} received data from process {i}/{numProcesses}.");
                     gatheredData.Add(receivedData);
                 }
-
-                Console.WriteLine($"Process {rank}/{numProcesses} received a lot of data as the result!");
-                
+                stopwatch.Stop();
+                Console.WriteLine($"Rank 0 gathered data from other processes. Elapsed Time: {stopwatch.ElapsedMilliseconds} ms");
             }
-
-            Communicator.world.Barrier(); // Synchronize all processes before exiting
-            DeleteTables(rank, numProcesses);
+            //
+            // Communicator.world.Barrier(); // Synchronize all processes before exiting
+            // DeleteTables(rank, numProcesses);
         }
     }
 
 }
-
-
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         using (new MPI.Environment(ref args))
-//         {
-//             if (Communicator.world.Rank != 0)
-//             {
-//                 int rank = Communicator.world.Rank;
-//
-//                 Console.WriteLine($"Process {rank} started.");
-//
-//                 // Load data
-//                 Stopwatch sw = new Stopwatch();
-//                 sw.Start();
-//                 List<ComputerHardware> allData = LoadData();
-//                 sw.Stop();
-//                 Console.WriteLine($"Process {rank} loaded data in {sw.ElapsedMilliseconds} ms.");
-//
-//                 // Divide the workload
-//                 int chunkSize = allData.Count / Communicator.world.Size;
-//                 int startIndex = rank * chunkSize;
-//                 int endIndex = (rank == Communicator.world.Size - 1) ? allData.Count : startIndex + chunkSize;
-//
-//                 // Process the local portion of the workload
-//                 sw.Reset();
-//                 sw.Start();
-//                 List<ComputerHardware> localData = allData.GetRange(startIndex, endIndex - startIndex);
-//                 List<string> results = ProcessData(localData);
-//                 sw.Stop();
-//                 Console.WriteLine($"Process {rank} processed data in {sw.ElapsedMilliseconds} ms.");
-//
-//                 // Send results to the master process
-//                 string serializedResults = JsonSerializerHelper.SerializeObject(results);
-//                 Console.WriteLine($"Process {rank} sending results to master process.");
-//                 Communicator.world.Send(serializedResults, 0, 0);
-//
-//                 Console.WriteLine($"Process {rank} done.");
-//             }
-//             else
-//             {
-//                 Console.WriteLine("Main process waiting to receive results:");
-//
-//                 List<string> allResults = new List<string>();
-//                 for (int i = 1; i < Communicator.world.Size; i++)
-//                 {
-//                     Console.WriteLine($"Main process waiting to receive results from process {i}.");
-//                     string receivedResults = Communicator.world.Receive<string>(i, 0);
-//                     List<string> deserializedResults =
-//                         JsonSerializerHelper.DeserializeObject<List<string>>(receivedResults);
-//                     allResults.AddRange(deserializedResults);
-//                 }
-//
-//                 Console.WriteLine("Aggregated Results:");
-//                 Console.WriteLine(allResults.Count);
-//             }
-//         }
-//     }
-//
-//     static List<ComputerHardware> LoadData()
-//     {
-//         using (var dbContext = new SampleDbContext())
-//         {
-//             return dbContext.ComputerHardwares.ToList();
-//         }
-//     }
-//
-//     static List<string> ProcessData(List<ComputerHardware> data)
-//     {
-//         List<string> results = new List<string>();
-//
-//         foreach (var item in data)
-//         {
-//             // Process each item and convert to a string
-//             string result = $"{item.Id}: {item.CPU}, {item.GPU}, {item.RAM}, {item.Motherboard}, {item.PSU}";
-//             results.Add(result);
-//         }
-//
-//         return results;
-//     }
-// }
